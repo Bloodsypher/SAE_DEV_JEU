1e9b0b928525c04efbeb28df506d71386ac34f2a
Colin Thomas <colin.thomas@etu.univ-smb.fr>
2023-01-04 17:36:19 +0100

    deplacement

--- Escape_The_Tower/Escape_The_Tower/Game1.cs
+++ Escape_The_Tower/Escape_The_Tower/Game1.cs
@@ -21,8 +21,8 @@ namespace Escape_The_Tower
         private KeyboardState _keyboardState;
         private int _vitessePerso;
         private float deltaSeconds;
-        public const int LONGUEUR_ECRAN = 1400;
-        public const int LARGEUR_ECRAN = 800;
+        public const int LONGUEUR_ECRAN = 1440;
+        public const int LARGEUR_ECRAN = 900;
         private TiledMapTileLayer mapLayer;
         private int _sensPersoX;
         private int _sensPersoY;
@@ -42,10 +42,11 @@ namespace Escape_The_Tower
             _graphics.PreferredBackBufferHeight = LARGEUR_ECRAN;
             _graphics.ApplyChanges();
 
-            _vitessePerso = 20;
+            _vitessePerso = 100;
 
             _positionPerso = new Vector2(20, 240);
 
+            _graphics.IsFullScreen = true;
             base.Initialize();
         }
 
@@ -147,8 +148,8 @@ namespace Escape_The_Tower
                 ushort txMillieu = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 1);
                 ushort tyMillieu = (ushort)(_positionPerso.Y / _tiledMap.TileHeight);
 
-                //ushort txHaut = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 1);
-                // ushort tyHaut = (ushort)(_positionPerso.Y / _tiledMap.TileHeight - 1);
+                ushort txHaut = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 1);
+                ushort tyHaut = (ushort)(_positionPerso.Y / _tiledMap.TileHeight - 1);
 
                 ushort txBas = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 1);
                 ushort tyBas = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 2);
@@ -156,6 +157,48 @@ namespace Escape_The_Tower
 
                 if (!IsCollision(txMillieu, tyMillieu) && !IsCollision(txBas, tyBas))// && !IsCollision(txHaut, tyHaut)
                     _sensPersoX = 1;
+            }
+            // si fleche gauche enfoncé
+            if (_keyboardState.IsKeyDown(Keys.Q) && !(_keyboardState.IsKeyDown(Keys.D)))
+            {
+                ushort txMillieu = (ushort)(_positionPerso.X / _tiledMap.TileWidth - 1);
+                ushort tyMillieu = (ushort)(_positionPerso.Y / _tiledMap.TileHeight);
+
+                ushort txHaut = (ushort)(_positionPerso.X / _tiledMap.TileWidth - 1);
+                ushort tyHaut = (ushort)(_positionPerso.Y / _tiledMap.TileHeight - 1);
+
+                ushort txBas = (ushort)(_positionPerso.X / _tiledMap.TileWidth - 1);
+                ushort tyBas = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 2);
+
+                if (!IsCollision(txMillieu, tyMillieu) && !IsCollision(txBas, tyBas))// && !IsCollision(txHaut, tyHaut)
+                    _sensPersoX = -1;
+            }
+
+            // si fleche haut enfoncé
+            if (_keyboardState.IsKeyDown(Keys.Z) && !(_keyboardState.IsKeyDown(Keys.S)))
+            {
+                ushort txGauche = (ushort)(_positionPerso.X / _tiledMap.TileWidth - 0.5);
+                ushort tyGauche = (ushort)((_positionPerso.Y + 10) / _tiledMap.TileHeight - 1);
+
+
+
+                ushort txDroite = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 0.5);
+                ushort tyDroite = (ushort)((_positionPerso.Y + 10) / _tiledMap.TileHeight - 1);
+                if (!IsCollision(txGauche, tyGauche) && !IsCollision(txDroite, tyDroite))
+                    _sensPersoY = -1;
+            }
+
+            // si fleche bas enfoncé
+            if (_keyboardState.IsKeyDown(Keys.S) && !(_keyboardState.IsKeyDown(Keys.Z)))
+            {
+                ushort txGauche = (ushort)(_positionPerso.X / _tiledMap.TileWidth - 0.5);
+                ushort tyGauche = (ushort)((_positionPerso.Y + 2) / _tiledMap.TileHeight + 2);
+
+                ushort txDroite = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 0.5);
+                ushort tyDroite = (ushort)((_positionPerso.Y + 2) / _tiledMap.TileHeight + 2);
+
+                if (!IsCollision(txGauche, tyGauche) && !IsCollision(txDroite, tyDroite))
+                    _sensPersoY = 1;
             }
 
             // deplace le personnage

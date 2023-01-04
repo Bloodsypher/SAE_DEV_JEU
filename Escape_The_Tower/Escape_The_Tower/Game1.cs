2a049f7598047d5455e0198fd4b3bb14a889b919
Colin Thomas <colin.thomas@etu.univ-smb.fr>
2023-01-04 17:25:40 +0100

    collision

--- Escape_The_Tower/Escape_The_Tower/Game1.cs
+++ Escape_The_Tower/Escape_The_Tower/Game1.cs
@@ -23,6 +23,10 @@ namespace Escape_The_Tower
         private float deltaSeconds;
         public const int LONGUEUR_ECRAN = 1400;
         public const int LARGEUR_ECRAN = 800;
+        private TiledMapTileLayer mapLayer;
+        private int _sensPersoX;
+        private int _sensPersoY;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -38,7 +42,7 @@ namespace Escape_The_Tower
             _graphics.PreferredBackBufferHeight = LARGEUR_ECRAN;
             _graphics.ApplyChanges();
 
-            _vitessePerso = 2;
+            _vitessePerso = 20;
 
             _positionPerso = new Vector2(20, 240);
 
@@ -50,9 +54,12 @@ namespace Escape_The_Tower
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            _tiledMap = Content.Load<TiledMap>("tutolab");
+            _tiledMap = Content.Load<TiledMap>("azer");
             _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
-           SpriteSheet spriteSheet = Content.Load<SpriteSheet>("persoAnimation.sf", new JsonContentLoader());
+
+            mapLayer = _tiledMap.GetLayer<TiledMapTileLayer>("collision");
+
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("persoAnimation.sf", new JsonContentLoader());
           _perso = new AnimatedSprite(spriteSheet);
         }
 
@@ -66,8 +73,9 @@ namespace Escape_The_Tower
             _perso.Play("idle"); // une des animations définies dans « persoAnimation.sf »
             _perso.Update(deltaSeconds); // time écoulé
 
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-
+            /*
             //DEPLACEMENT PERSO1
             _keyboardState = Keyboard.GetState();
 
@@ -127,6 +135,32 @@ namespace Escape_The_Tower
                 _positionPerso.Y = _positionPerso.Y - _vitessePerso;
                 _positionPerso.X = _positionPerso.X - _vitessePerso;
             }
+            */
+            _keyboardState = Keyboard.GetState();
+            _sensPersoX = 0;
+            _sensPersoY = 0;
+
+            // si fleche droite enfoncé
+            if (_keyboardState.IsKeyDown(Keys.D))
+            {
+                Console.WriteLine("dzd");
+                ushort txMillieu = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 1);
+                ushort tyMillieu = (ushort)(_positionPerso.Y / _tiledMap.TileHeight);
+
+                //ushort txHaut = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 1);
+                // ushort tyHaut = (ushort)(_positionPerso.Y / _tiledMap.TileHeight - 1);
+
+                ushort txBas = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 1);
+                ushort tyBas = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 2);
+
+
+                if (!IsCollision(txMillieu, tyMillieu) && !IsCollision(txBas, tyBas))// && !IsCollision(txHaut, tyHaut)
+                    _sensPersoX = 1;
+            }
+
+            // deplace le personnage
+            _positionPerso.X += _sensPersoX * _vitessePerso * deltaTime;
+            _positionPerso.Y += _sensPersoY * _vitessePerso * deltaTime;
 
             base.Update(gameTime);
         }
@@ -144,6 +178,16 @@ namespace Escape_The_Tower
             base.Draw(gameTime);
         }
 
+        private bool IsCollision(ushort x, ushort y)
+        {
+            // définition de tile qui peut être null (?)
+            TiledMapTile? tile;
+            if (mapLayer.TryGetTile(x, y, out tile) == false)
+                return false;
+            if (!tile.Value.IsBlank)
+                return true;
+            return false;
+        }
     }
 
 }

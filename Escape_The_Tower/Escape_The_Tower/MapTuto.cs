f9d25971f963db8f8e965105a89d5721e9d11cb8
Colin Thomas <colin.thomas@etu.univ-smb.fr>
2023-01-10 15:11:21 +0100

    sfgd

--- Escape_The_Tower/Escape_The_Tower/MapTuto.cs
+++ Escape_The_Tower/Escape_The_Tower/MapTuto.cs
@@ -77,7 +77,7 @@ namespace Escape_The_Tower
             //Initialise sprite obj
             _textutePorte = Content.Load<Texture2D>("porte1");
             _texturePlaque = Content.Load<Texture2D>("plaque_de_pression");
-            _textutePorteOuverte = Content.Load<Texture2D>("porte");
+            _textutePorteOuverte = Content.Load<Texture2D>("porte1");
             SpriteSheet spriteSheetfeu = Content.Load<SpriteSheet>("fire.sf", new JsonContentLoader());
 
             //feu
